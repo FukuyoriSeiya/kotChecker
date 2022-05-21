36d959e73992df85ae3c5052baf2750279801a6a
SeiyaFUkuyori <seiya.fukuyori@gmail.com>
2022-05-21 17:05:32 +0900

    ページのHTMLを取得 <rect>を抽出する必要あり

---
+++ const.py
@@ -0,0 +1,4 @@
+EMAIL = 'seiya.fukuyori@gmail.com'
+PASSWORD = 'FUkuyoRI12'
+UNAME = 'FukuyoriSeiya'
+TOKEN = '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'
--- scraping.py
+++ scraping.py
@@ -1,6 +1,30 @@
 from selenium import webdriver
-from time import sleep
+import time
+import const
 
-
+# ログインページに遷移
 driver = webdriver.Chrome('C:\Chrome\chromedriver.exe')
-driver.get('https://mail.google.com/mail/u/0/')
+driver.get('https://github.com/login')
+
+time.sleep(1)
+
+# ログイン処理
+mail = driver.find_element_by_name('login')
+password = driver.find_element_by_name('password')
+
+mail.clear()
+password.clear()
+
+mail.send_keys(const.EMAIL)
+password.send_keys(const.PASSWORD)
+
+mail.submit()
+
+# マイページに遷移
+driver.get('https://github.com/' + const.UNAME)
+
+# テーブル内容取得
+elem = driver.find_element_by_xpath("//*")
+source_code = elem.get_attribute("outerHTML")
+
+print(source_code)
